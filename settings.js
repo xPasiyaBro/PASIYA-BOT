0adb5ac820db04749702aa587f77bcc86410b67b
xPasiyaBro <110656205+xPasiyaBro@users.noreply.github.com>
2022-11-27 21:25:14 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -36,7 +36,7 @@ global.socialm = "GitHub: pasiya_lk" // github or insta name
 global.location = "India, Mizoram, Aizawl" // location
 
 //bot body 
-global.alivepic = `https://i.ibb.co/cyyCjN7/20220803-081426.png` // alive picture \\ use "https://ibb.com" to upload photos. < 1 MB
+global.alivepic = `https://i.ibb.co/FB5yZkc/pngtree-illustration-of-a-beautiful-and-sexy-girl-png-image-2213155.jpg` // alive picture \\ use "https://ibb.com" to upload photos. < 1 MB
 
 global.dragonchat = "false"  // chat bot  "true" / "false" (200 auto replies in this)
 global.callblock = "false" // call block "true" / "false" ("true"  to block callers)
